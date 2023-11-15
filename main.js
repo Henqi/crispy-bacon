bc0041ff30fe8b461bbd83434662dea856bd45aa
Henri Heilala <contact@henriheilala.fi>
2023-11-15 22:45:55 +0200

    feature: first working version

---
+++ main.js
@@ -0,0 +1,15 @@
+const Bacon = require('baconjs');
+
+// Create a Bacon.js stream
+const myStream = Bacon.fromArray([1, 2, 3, 4, 5]);
+
+// Define a transformation on the stream
+const transformedStream = myStream.map(x => x * 2);
+
+// Subscribe to the transformed stream
+transformedStream.onValue(value => {
+  console.log(value);
+});
+
+let s = Bacon.once("heiia")
+s.log()
